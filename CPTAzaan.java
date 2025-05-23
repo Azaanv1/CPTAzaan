0ee41b319831ef32758fa80c063ef5f5b0ebf4d4
Azaanv1 <azaan.virani27@ycdsbk12.ca>
2025-05-23 09:32:46 -0400

    Done

--- CPTAzaan.java
+++ CPTAzaan.java
@@ -4,5 +4,8 @@ public class CPTAzaan {
     public static void main(String[] args) {
         Console con = new Console();
         con.println("Hello World");
+        con.println("TESTING");
+        con.println("CPT");
+        con.println("ICS3U1");
 	}
 }
