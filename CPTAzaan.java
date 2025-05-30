2a87e09841187c87b82929f4699eab647763d025
Azaanv1 <azaan.virani27@ycdsbk12.ca>
2025-05-30 09:38:11 -0400

    completed main menu

--- CPTAzaan.java
+++ CPTAzaan.java
@@ -1,13 +1,46 @@
 import arc.*;
-
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
 public class CPTAzaan {
     public static void main(String[] args) {
-        Console con = new Console();
+		Console con = new Console();
 		// create master file called themes.txt
 		// Inside master you will need a bunch of diffrent themes
 		
-		String strName;
-		//Array variable 
+		int intX = 400;
+        int intY = 400;
+        int intZ = 400;
+        
+        while(intX > 0){
+			con.setDrawColor(Color.BLACK);
+			con.fillRect(0,0,700,700);
+			con.setDrawColor(Color.WHITE);
+		// Setting a predifined color
+		con.setDrawColor(Color.RED);
+		intZ = intZ -9;
+		// Setting a custom red, blue, green rgb color 0-255
+		con.setDrawColor(new Color(3, 100, 230));
+		intY = intY + 5;
+		BufferedImage 01_Menu = con.loadImage("01_Menu.png");
+		Font fntTest = con.loadFont("Play Now", 40);
+		con.drawImage(01_Menu, intX, 0);
+		intX = intX - 8;
+		
+		con.setDrawFont(fntTest);
+		
+		con.setDrawColor(new Color(200, 30, 30));
+		con.drawString("Quit!", 0,0);
+		// repainting ensures that all the drawing commands above appear
+		con.repaint();
+		con.sleep(33);
+		
+		
+
+			
+			
+		
+	}
       
       
       
