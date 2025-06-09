63a98f0695423ccf6e37099a406f14be3c61f5a7
Azaanv1 <azaan.virani27@ycdsbk12.ca>
2025-06-09 17:33:09 -0400

    Started New Code

---
+++ CPTAzaan.java
@@ -0,0 +1,11 @@
+import arc.*;
+
+public class CPTAzaan {
+    public static void main(String[] args) {
+        Console con = new Console();
+        con.println("Hello World");
+        con.println("Final CPT");
+        
+    }
+}
+
